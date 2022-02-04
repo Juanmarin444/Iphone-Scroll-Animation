3d7dc184240d13c0d0abad1d4147e0b0de97f298
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-02-03 23:08:25 -0800

    moved mobile nav animations to main project

--- nav-app.js
+++ nav-app.js
@@ -1,9 +1,41 @@
 const chevron = document.querySelector('.chevron');
-const navLinks = document.querySelector('.nav-links');
 
-let navStatus = "desktop"
+let navStatus = "closed"
+
+const mNavTl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" }})
 
 chevron.addEventListener('click', (e) => {
-  gsap.fromTo('nav', { background: 'rgba(255,255,255, .6)'}, {background: 'rgba(255,255,255, .85)' })
-  gsap.fromTo('.mobile', { display: 'none', height: '0', background: 'rgba(255,255,255, .6)' }, { display: 'flex', height: '156px', justifyContent: 'space-evenly', background: 'rgba(255,255,255, .85)' }, '<')
+  if (navStatus === "opened") {
+
+    mNavTl.fromTo('.cover', { opacity: .4 }, { opacity: 0 });
+    mNavTl.fromTo('.mobile a', {y: -10, opacity: 1}, {y: -40, opacity: 0, stagger: .1, duration: .50, ease: "power2.out"}, '<');
+
+
+    mNavTl.fromTo('nav', { background: 'rgba(255,255,255, .85)'}, {background: 'rgba(255,255,255, .6)' });
+
+    mNavTl.fromTo('.mobile', {
+      display: 'flex', height: '156px', justifyContent: 'space-evenly', background: 'rgba(255,255,255, .85)'
+    }, {
+      display: 'none', height: '0', background: 'rgba(255,255,255, .6)'
+    }, '<');
+
+
+    navStatus = "closed"
+  } else if (navStatus === "closed") {
+
+
+    mNavTl.fromTo('nav', { background: 'rgba(255,255,255, .6)'}, {background: 'rgba(255,255,255, .85)' });
+
+    mNavTl.fromTo('.mobile', {
+      display: 'none', height: '0', background: 'rgba(255,255,255, .6)'
+    }, {
+      display: 'flex', height: '156px', justifyContent: 'space-evenly', background: 'rgba(255,255,255, .85)'
+    }, '<');
+
+    mNavTl.fromTo('.cover', { opacity: 0 }, { opacity: .4 });
+
+    mNavTl.fromTo('.mobile a', {y: -40, opacity: 0}, {y: -10, opacity: 1, stagger: .1, duration: .50, ease: "power2.out"}, '<');
+
+    navStatus = "opened"
+  }
 })
